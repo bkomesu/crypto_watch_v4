f68233aac2819ec1527b91933c4c3ab26582b73d
Bruce Komesu <bkomesu2@gmail.com>
2021-12-19 12:58:59 -0600

    made chart

--- src/pages/CoinDetailPage.js
+++ src/pages/CoinDetailPage.js
@@ -1,36 +1,81 @@
-import React, {useEffect} from "react";
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import HistoryChart from "../Components/HistoryChart";
 import CoinData from "../Components/CoinData";
 import coinGecko from "../apis/coinGecko";
 
 const CoinDetailPage = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [coinData, setCoinData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const results = await coinGecko.get(`/coins/${id}/market_chart/`, {
-                params: {
-                    vs_currency: "usd",
-                    days: "1",
-                },
-            });
+  const formatData = (data) => {
+    return data.map((el) => {
+      return {
+        x: el[0],
+        y: el[1].toFixed(2),
+      };
+    });
+  };
 
-            console.log(results.data);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const [day, week, year, detail] = await Promise.all([
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "1",
+          },
+        }),
 
-        fetchData();
-       }, []);
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "7",
+          },
+        }),
 
-        const renderData = () => {
-            return (
-                <div className="coinlist">
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: "usd",
+            days: "365",
+          },
+        }),
 
-                </div>
-            );
-        };
+        coinGecko.get("/coins/markets/", {
+          params: {
+            vs_currency: "usd",
+            ids: id,
+          },
+        }),
+      ]);
 
-        return renderData();
+      setCoinData({
+        day: formatData(day.data.prices),
+        week: formatData(week.data.prices),
+        year: formatData(year.data.prices),
+        detail: detail.data[0],
+      });
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const renderData = () => {
+    if (isLoading) {
+      return <div>Loading...</div>;
     }
+    return (
+      <div className="coinlist">
+        <HistoryChart data={coinData} />
+        <CoinData />
+      </div>
+    );
+  };
 
-export default CoinDetailPage
+  return renderData();
+};
+
+export default CoinDetailPage;
